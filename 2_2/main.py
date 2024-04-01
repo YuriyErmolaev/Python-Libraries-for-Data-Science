3f86201cdf8206c3cb9f6d74af12d0351cabefe3
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2024-04-01 13:16:43 +0700

    read sbscrs and users

--- 2_2/main.py
+++ 2_2/main.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 m_camp_path = './input_data/marketing_campaign.csv'
-
 m_camp_df = pd.read_csv(m_camp_path)
 
+sbscrbs_path = './input_data/subscribers.csv'
+sbscrbs_df = pd.read_csv(sbscrbs_path)
+
+users_path = './input_data/users.csv'
+users_df = pd.read_csv(users_path)
+
 print(m_camp_df)
+print(sbscrbs_df)
+print(users_df)
